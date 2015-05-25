3ba98897f16f58a1b17178e864ef48f867b2598d
Scott Robertson <scott@scottusrobus.com>
2015-05-25 08:53:52 -0700

    add foundation package

---
+++ packages/vhx:foundation/package.js
@@ -0,0 +1,18 @@
+Package.describe({
+  name: 'vhx:foundation',
+  summary: 'VHX Foundation Custom Install',
+  version: '0.1.0'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.2');
+
+  api.use([]);
+
+  api.addFiles([
+    'foundation.css'
+  ]);
+
+  api.export([]);
+
+});
