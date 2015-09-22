2b49cda9a6ca279a28d21c7f26738f6545d82d82
Scott Robertson <scott@scottusrobus.com>
2015-09-22 16:03:53 -0400

    bump version, update bower json

--- .meteor/local/build/programs/web.browser/app/client/layouts/template.head.js
+++
@@ -1,8 +0,0 @@
-(function(){
-Template.body.addContent((function() {
-  var view = this;
-  return "";
-}));
-Meteor.startup(Template.body.renderToDocument);
-
-})();
--- .meteor/local/build/programs/web.browser/app/client/views/template.view.root.js
+++
@@ -1,8 +0,0 @@
-(function(){
-Template.__checkName("home");
-Template["home"] = new Template("Template.home", (function() {
-  var view = this;
-  return "home";
-}));
-
-})();
