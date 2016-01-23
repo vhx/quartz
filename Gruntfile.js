9d9e30acc47e10248ad5f79ea28986accd42ebc3
Scott Robertson <scott@scottusrobus.com>
2016-01-22 23:01:55 -0800

    grunt folder setup for new icon flow

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,11 +6,8 @@ module.exports = function(grunt) {
         files : {
           'app/packages/vhx.scss': [
             'app/packages/**/styles/*.scss',
-            '!app/packages/vhx-style-icons/styles/icons.extends.scss',
-            '!app/packages/vhx-style-icons/styles/icons.fallback.scss',
             '!app/packages/vhx-style-icons/styles/icons.svg.scss',
             '!app/packages/**/styles/guide.scss',
-            '!app/packages/vhx-style-icons/styles/icons.png.scss',
             '!app/packages/_vhx-style-template/**/*.scss'
           ]
         }
@@ -30,73 +27,37 @@ module.exports = function(grunt) {
         }
       }
     },
-    clean: ['app/private/svg-icons-renamed/', 'app/private/svg-icons-minified/'],
-    fileregexrename: {
-      multiColorIcons: {
-        files: {
-          'app/private/svg-icons-renamed/*.svg' : 'app/private/svg-icons-source/*.svg'
-        },
-        options: {
-          replacements: [{
-            pattern: /icon-set-multi-colored-cs6_/ig,
-            replacement: ''
-          }, {
-            pattern: /\.svg/ig,
-            replacement: '.colors-navy-gray-white-teal.svg'
-          }]
-        }
-      }
+    concat: {
+      svg: {
+        src: 'app/private/svg-css/all/*.css',
+        dest: 'app/packages/vhx-style-icons/styles/icons.svg.scss'
+      },
     },
-    svgmin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: 'app/private/svg-icons-renamed',
-          src: ['*.svg'],
-          dest: 'app/private/svg-icons-minified'
-        }]
-      }
-    },
-    grunticon: {
-      multiColor: {
-        files: [{
-          expand: true,
-          cwd: 'app/private/svg-icons-minified',
-          src: ['*.svg', '*.png'],
-          dest: 'app/packages/vhx-style-icons/styles'
-        }],
-        options: {
-          datasvgcss: 'icons.svg.scss',
-          datapngcss: 'icons.png.scss',
-          urlpngcss: 'icons.fallback.scss',
-          cssprefix: '.icon-',
-          pngfolder: 'png-icons-source',
-          // TODO: this is path to output to output PNGs
-          pngpath: 'png-icons-output',
-          colors: {
-            navy: '#375667',
-            gray: '#9baab2',
-            white: '#ffffff',
-            teal: '#22b8af'
-          }
-        }
-      }
+    clean: {
+      svg: ['app/private/svg-css/all/*.css', 'app/private/svg-css/sets/*.css']
     },
     copy: {
-      iconExtends: {
-        src: 'app/packages/vhx-style-icons/styles/icons.svg.scss',
-        dest: 'app/packages/vhx-style-icons/styles/icons.extends.scss',
-          options: {
-            process: function(content, srcpath) {
-              return content.replace(/^\.icon/gm,'%icon');
-            }
-          }
-      },
       css: {
         cwd: 'app/private/quartz-rails/vendor/assets/stylesheets/',
         src: '**/*',
         dest: 'app/private/distro/',
         expand: true
+      },
+      svg: {
+        files: [
+          {
+            src: 'app/private/svg-css/**/*.css',
+            dest: 'app/private/distro/',
+            flatten: true,
+            expand: true
+          },
+          {
+            src: 'app/private/svg-css/**/*.css',
+            dest: 'app/private/quartz-rails/vendor/assets/stylesheets/',
+            flatten: true,
+            expand: true
+          }
+        ]
       }
     },
     folder_list: {
@@ -120,9 +81,14 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-folder-list');
+  grunt.registerTask('grunt-svg-css', function() {
+    var done = this.async();
+    var SVG = require('./app/private/scripts/svg.task')(done);
+  });
 
-  grunt.registerTask('build-icons', ['clean', 'fileregexrename:multiColorIcons', 'svgmin', 'grunticon:multiColor']);
+  grunt.registerTask('build-icons', ['grunt-svg-css', 'copy:svg', 'concat:svg', 'clean:svg']);
   grunt.registerTask('build', ['sass_globbing', 'cssmin', 'copy:css', 'copy:iconExtends', 'sass']);
   grunt.registerTask('files', ['folder_list']);
 };
