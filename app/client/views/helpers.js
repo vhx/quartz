6aafc0fafbe02f6614ea6ab82b9289a97cbebbfc
Scott Robertson <scott@scottusrobus.com>
2016-01-28 22:11:09 -0800

    add mithril docs/conventions wip

--- app/client/views/helpers.js
+++ app/client/views/helpers.js
@@ -68,10 +68,14 @@ Template.js_mithril_conventions.helpers({
   jsMithril: function() {
     return [
       { guide: 'guide__js_mithril_intro', code:'code__js_mithril_intro' },
-      { guide: 'guide__js_mithril_namespace', code:'code__js_mithril_namespace' },
       { guide: 'guide__js_mithril_folders', code:'code__js_mithril_folders' },
-      { guide: 'guide__js_mithril_components_org', code:'code__js_mithril_components_org' },
-      { guide: 'guide__js_mithril_ui', code:'code__js_mithril_ui' }
+      { guide: 'guide__js_mithril_order', code:'code__js_mithril_order' },
+      { guide: 'guide__js_mithril_namespace', code:'code__js_mithril_namespace' },
+      { guide: 'guide__js_mithril_layouts', code:'code__js_mithril_layouts' },
+      { guide: 'guide__js_mithril_components', code:'code__js_mithril_components' },
+      { guide: 'guide__js_mithril_views', code:'code__js_mithril_views' },
+      { guide: 'guide__js_mithril_models', code:'code__js_mithril_models' },
+      { guide: 'guide__js_mithril_requests', code:'code__js_mithril_requests' }
     ];
   }
 });
