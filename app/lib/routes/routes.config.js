b9f94b0f353ac73124aa79ece5b765db0220f3c4
Scott Robertson <scott@scottusrobus.com>
2016-05-23 19:47:30 -0700

    remove meteor routes

--- app/lib/routes/routes.config.js
+++
@@ -1,7 +0,0 @@
-Router.configure({
-  layoutTemplate: 'app_layout',
-  onBeforeAction: function() {
-    $('body').scrollTop(0);
-    this.next();
-  }
-});
--- app/lib/routes/routes.root.js
+++
@@ -1,8 +0,0 @@
-/* ROOT ROUTES
-.................................................*/
-
-// home
-Router.route('/', {
-  name: 'home',
-  controller: 'Home'
-});
