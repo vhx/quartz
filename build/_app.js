bdb234bcbc30720527422be3fdc6ebb9f85d3453
Scott Robertson <scott@scottusrobus.com>
2016-06-13 22:41:08 -0700

    add manifests

---
+++ build/_app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let Manifest = [
+  'app/client/ext.js',
+  'app/client/scope.js',
+
+  /* models */
+  'app/client/models/scripts.js',
+  'app/client/models/styles.js',
+
+  /* layouts */
+  'app/client/layouts/layouts.standard.controller.js',
+  'app/client/layouts/layouts.standard.state.js',
+  'app/client/layouts/layouts.standard.ui.js',
+
+  /* components */
+  'app/client/components/guide/block.ui.js',
+  'app/client/components/guide/intro.ui.js',
+  'app/client/components/guide/color.ui.js',
+  'app/client/components/guide/route.elements.ui.js',
+  'app/client/components/guide/route.typography.ui.js',
+
+  /* routes */
+  'app/client/routes.js'
+];
+
+module.exports = Manifest;
---
+++ build/_vendor.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let Manifest = [
+  'node_modules/underscore/underscore.js',
+  'node_modules/mithril/mithril.js',
+  'node_modules/highlight.js/lib/highlight.js'
+];
+
+module.exports = Manifest;
