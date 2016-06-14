77e389b41f81b6d5ce55b11f1cd2934d22c67c5a
Scott Robertson <scott@scottusrobus.com>
2016-06-13 22:43:54 -0700

    walk through all the docs files

--- app/server/ignite.js
+++ app/server/ignite.js
@@ -9,16 +9,35 @@ const http        = require('http');
 const parser      = require('body-parser');
 const handlebars  = require('handlebars');
 const fs          = require('fs');
+const walk        = require('fs-walk');
 const app         = express();
+const views_dir   = 'quartz-css';
 
 app.set('port', process.env.PORT);
 app.use(express.static('app/public'));
 app.use(parser.json());
 
+
+/* Register Views as Partials
+..............................*/
+let views = '';
+let code = {};
 let menu_template = fs.readFileSync(__dirname + '/layouts/menu.html', 'utf8');
 let home_template = fs.readFileSync(__dirname + '/layouts/home.html', 'utf8');
 handlebars.registerPartial('menu', menu_template);
 handlebars.registerPartial('home', home_template);
+
+walk.walkSync(views_dir, function(base_dir, filename) {
+  if (base_dir.match(/\/docs/) && filename.match(/\.html\.js/)) {
+    let template = fs.readFileSync(base_dir + '/' + filename, 'utf8');
+    views = views + template;
+  }
+  if (base_dir.match(/\/docs\/code/)) {
+    let temp = fs.readFileSync(base_dir + '/' + filename, 'utf8');
+    let name = filename.split('.')[0];
+    code[name] = JSON.stringify(temp);
+  }
+});
 
 app.get('*', function(request, response) {
   fs.readFile(__dirname + '/layouts/app.html', 'utf8', function(err, source){
@@ -26,7 +45,11 @@ app.get('*', function(request, response) {
       return console.log(err);
     }
 
-    response.send(handlebars.compile(source)({ index: request.url === '/' }));
+    response.send(handlebars.compile(source)({
+      index: request.url === '/',
+      views: views,
+      code: JSON.stringify(code)
+    }));
   });
 });
 
