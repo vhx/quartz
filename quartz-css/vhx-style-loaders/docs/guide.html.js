4cd82758f1e5ad7a5f8fb49d9cff4b511f4ace6b
Scott Robertson <scott@scottusrobus.com>
2016-06-14 15:52:17 -0700

    mithrilize loaders

---
+++ quartz-css/vhx-style-loaders/docs/guide.html.js
@@ -0,0 +1,38 @@
+Q.components.guide.styleguide.ui.text = {};
+
+Q.components.guide.styleguide.ui.loaders.container = {
+  view: function(ctrl) {
+    return m('section#guide--style-loaders', [
+      m.component(Q.components.shared.guide.intro.ui.container, {
+        title: 'Loaders'
+      }),
+      m.component(Q.components.shared.guide.block.ui.container, {
+        title: 'Core Colors',
+        component: {
+          view: function() {
+            return m('div', [
+              m('span.loader-slate'),
+              m('span.loader-teal'),
+              m('span.bg-grid-dark.inline', [
+                m('span.loader-white')
+              ])
+            ]);
+          }
+        }
+      }),
+      m.component(Q.components.shared.guide.block.ui.container, {
+        title: 'Sizes',
+        component: {
+          view: function() {
+            return m('div', [
+              m('span.loader-slate.loader--small'),
+              m('span.loader-slate.loader--medium'),
+              m('span.loader-slate.loader--large'),
+              m('span.loader-slate.loader--xlarge')
+            ]);
+          }
+        }
+      })
+    ]);
+  }
+};
