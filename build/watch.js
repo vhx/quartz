03111ab72a88dffdbd9d2645d46f061cb9519a80
Scott Robertson <scott@scottusrobus.com>
2016-06-14 23:17:37 -0700

    add quartz js build/watch script

--- build/watch.js
+++ build/watch.js
@@ -2,13 +2,15 @@
 
 const catw        = require('catw');
 const fs          = require('fs');
+const walk        = require('fs-walk');
 const chalk       = require('chalk');
 const babel       = require('babel-core');
 const sass        = require('node-sass');
 const concat      = require('concat-files');
 const manifests   = {
   scripts: require(__dirname + '/_app.js'),
-  vendor: require(__dirname + '/_vendor.js')
+  vendor: require(__dirname + '/_vendor.js'),
+  quartzjs: require(__dirname + '/_quartzjs.js')
 };
 
 catw('app/client/**/*.js', function() {
@@ -68,6 +70,30 @@ catw('quartz-css/**/*.scss', function() {
     fs.writeFile('app/public/quartz.css', output.css, function(err) {
     if (err) { process.stdout.write(chalk.red(err)); }
       process.stdout.write(chalk.green('CSS Updated \n'));
+    });
+  });
+});
+
+catw('quartz-js/**/*.js', function() {
+  let src = 'app/public/quartz.js';
+
+  walk.walkSync('quartz-js/components', function(base_dir, filename, stat) {
+    if (!stat.isDirectory() && !filename.match(/scope\.js/)) {
+      manifests.quartzjs.push(base_dir + '/' + filename);
+    }
+  });
+
+  concat(manifests.quartzjs, src, function() {
+    let result = babel.transform(fs.readFileSync(src).toString(), {
+      'presets': ['es2015']
+    });
+    fs.writeFile(src, result.code, function(err) {
+      if (err) {
+        process.stdout.write(chalk.red(err));
+      }
+      else {
+        process.stdout.write(chalk.yellow('Quartz JS Updated \n'));
+      }
     });
   });
 });
