c36324f05c13d6008095bb5e967af4e73499cdc1
Scott Robertson <scott@scottusrobus.com>
2016-06-17 12:46:56 -0700

    latest app.js build

--- app/public/app.js
+++ app/public/app.js
@@ -157,6 +157,17 @@ Q.components.shared.guide.color.ui.container = {
     guide: m.component(Q.components.guide.styleguide.ui.buttons.container),
     code: Q.components.code.buttons
   }]
+};Q.components.route.styleguide.ui.utilities = {
+  items: [{
+    guide: m.component(Q.components.guide.styleguide.ui.display.container),
+    code: Q.components.code.display
+  }, {
+    guide: m.component(Q.components.guide.styleguide.ui.grid.container),
+    code: Q.components.code.grid
+  }, {
+    guide: m.component(Q.components.guide.styleguide.ui.spacing.container),
+    code: Q.components.code.spacing
+  }]
 };Q.components.route.styleguide.ui.typography = {
   items: [{
     guide: m.component(Q.components.guide.styleguide.ui.text.container),
