99477a8825a306a6f64ca84ae4c105bdcecc80f7
Scott Robertson <scott@scottusrobus.com>
2016-06-17 07:44:42 -0700

    add a scope.js file

---
+++ app/public/scope.js
@@ -0,0 +1,24 @@
+window.Q = {
+  models: {
+    data: {},
+  },
+  components: {
+    shared: {
+      guide: {
+        intro: { ui: {} },
+        block: { ui: {} },
+        color: { ui: {} }
+      }
+    },
+    guide: {
+      styleguide: {
+        ui: {}
+      }
+    }
+  },
+  layouts: {
+    standard: {
+      ui: {}
+    }
+  }
+};
