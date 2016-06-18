b8540c0e3c0bca97d831be11e65d7481196b66a2
Scott Robertson <scott@scottusrobus.com>
2016-06-18 11:27:00 -0700

    setup scaffolding for undocumented css element styles

--- quartz-css/vhx-style-lists/docs/guide.html.js
+++ quartz-css/vhx-style-lists/docs/guide.html.js
@@ -2,7 +2,7 @@ Q.components.guide.styleguide.ui.lists = {};
 
 Q.components.guide.styleguide.ui.lists.container = {
   view: function(ctrl) {
-    return m('section#guide--style-text', [
+    return m('section#guide--style-lists', [
       m.component(Q.components.shared.intro.ui.container, {
         title: 'Lists'
       })
