c97175086acaa1ca97a3320f1c7d06d57f5b3c8c
Scott Robertson <scott@scottusrobus.com>
2016-06-18 09:59:11 -0700

    update build/grunt scripts with new references

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,13 +4,13 @@ module.exports = function(grunt) {
     sass_globbing: {
       target : {
         files : {
-          'app/packages/vhx.scss': [
-          'app/packages/**/styles/*.scss',
-          '!app/packages/vhx-style-icons/styles/icons.svg.scss',
-          '!app/packages/vhx-style-icons/styles/icons.extends.scss',
-          '!app/packages/vhx-style-colors/styles/colors.scss',
-          '!app/packages/**/styles/guide.scss',
-          '!app/packages/_vhx-style-template/**/*.scss'
+          'quartz-css/vhx.scss': [
+          'quartz-css/**/styles/*.scss',
+          '!quartz-css/vhx-style-icons/styles/icons.svg.scss',
+          '!quartz-css/vhx-style-icons/styles/icons.extends.scss',
+          '!quartz-css/vhx-style-colors/styles/colors.scss',
+          '!quartz-css/**/styles/guide.scss',
+          '!quartz-css/_vhx-style-template/**/*.scss'
           ]
         }
       }
@@ -18,27 +18,27 @@ module.exports = function(grunt) {
     sass: {
       dist: {
         files : {
-          'app/private/quartz-rails/vendor/assets/stylesheets/vhx-quartz.css' : 'app/packages/vhx.scss'
+          'quartz-rails/vendor/assets/stylesheets/vhx-quartz.css' : 'quartz-css/vhx.scss'
         }
       }
     },
     cssmin: {
       target : {
         files : {
-          'app/private/quartz-rails/vendor/assets/stylesheets/vhx-quartz.min.css' : 'app/private/quartz-rails/vendor/assets/stylesheets/vhx-quartz.css'
+          'quartz-rails/vendor/assets/stylesheets/vhx-quartz.min.css' : 'quartz-rails/vendor/assets/stylesheets/vhx-quartz.css'
         }
       }
     },
     concat: {
       svg: {
-        src: 'app/private/svg-css/all/*.css',
-        dest: 'app/packages/vhx-style-icons/styles/icons.svg.scss'
+        src: 'quartz-svg/svg-css/all/*.css',
+        dest: 'quartz-css/vhx-style-icons/styles/icons.svg.scss'
       },
     },
     clean: {
-      svg: ['app/private/svg-css/all/*.css', 'app/private/svg-css/sets/*.css'],
+      svg: ['quartz-svg/svg-css/all/*.css', 'quartz-svg/svg-css/sets/*.css'],
       pre_svg: {
-        src: ['app/private/distro/*.css', 'app/private/quartz-rails/vendor/assets/stylesheets/*.css'],
+        src: ['distro/*.css', 'quartz-rails/vendor/assets/stylesheets/*.css'],
         filter: function(filepath) {
           var regex = /(vhx-quartz.icon)(.+)(.css)/g;
           return regex.test(filepath);
@@ -47,22 +47,22 @@ module.exports = function(grunt) {
     },
     copy: {
       css: {
-        cwd: 'app/private/quartz-rails/vendor/assets/stylesheets/',
+        cwd: 'quartz-rails/vendor/assets/stylesheets/',
         src: '**/*',
-        dest: 'app/private/distro/',
+        dest: 'distro/',
         expand: true
       },
       svg: {
         files: [
           {
-            src: 'app/private/svg-css/**/*.css',
-            dest: 'app/private/distro/',
+            src: 'quartz-svg/svg-css/**/*.css',
+            dest: 'distro/',
             flatten: true,
             expand: true
           },
           {
-            src: 'app/private/svg-css/**/*.css',
-            dest: 'app/private/quartz-rails/vendor/assets/stylesheets/',
+            src: 'quartz-svg/svg-css/**/*.css',
+            dest: 'quartz-rails/vendor/assets/stylesheets/',
             flatten: true,
             expand: true
           }
@@ -75,9 +75,9 @@ module.exports = function(grunt) {
         folders: true
       },
       files: {
-        src: ['private/svg/all/*.svg'],
-        dest: 'app/packages/vhx-style-icons/docs/icon-list.json',
-        cwd: 'app/'
+        src: ['quartz-svg/svg/all/*.svg'],
+        dest: 'vhx-style-icons/docs/icon-list.json',
+        cwd: './'
       }
     }
   });
@@ -94,7 +94,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-folder-list');
   grunt.registerTask('grunt-svg-css', function() {
     var done = this.async();
-    var SVG = require('./app/private/scripts/svg.task')(done);
+    var SVG = require('./build/svg')(done);
   });
 
   grunt.registerTask('build-icons', ['clean:pre_svg', 'grunt-svg-css', 'copy:svg', 'concat:svg', 'clean:svg']);
