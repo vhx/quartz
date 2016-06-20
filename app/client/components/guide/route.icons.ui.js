ba8e5284257dbd6d1891868cbb044a6d8fd48541
Scott Robertson <scott@scottusrobus.com>
2016-06-20 07:15:12 -0700

    add icon styles to route

--- app/client/components/guide/route.icons.ui.js
+++ app/client/components/guide/route.icons.ui.js
@@ -2,6 +2,10 @@
 Q.components.route.styleguide.ui.icons = {
   items: [
     {
+      guide: m.component(Q.components.guide.styleguide.ui.icons_styles.container),
+      code: Q.code['icon-styles']
+    },
+    {
       guide: m.component(Q.components.guide.styleguide.ui.icons_names.container),
       code: Q.code['icon-names']
     },
