b1725cebe695d64f50d4dda548bfcb5d28ebf9bc
Steven Bone <steven@stevenbone.com>
2016-06-24 15:25:20 -0400

    Sample Select component code

--- quartz-js/components/select/docs/code/select.js
+++ quartz-js/components/select/docs/code/select.js
@@ -1,19 +1,18 @@
-// Example Filter Component
+// Example Select Component
 
-m.component(vhxm.components.shared.filter.container, {
-  label: 'Customize this table',
-  api: vhxm.component.name.state.filterApi,
-  filters: [
-    'date',
-    'location',
-    {
-      type: 'test',
-      title: 'Test',
-      template: {
-        view: function() {
-          // custom filter template
-        }
-      }
-    }
-  ]
+m.component(vhxm.components.shared.select.ui.container, {
+  search: mySearch(query, callback),
+  selected: {
+    value: null,
+    label: 'Nothing Selected'
+  },
+  items: vhxm.models.section.items,
+  value_prop: 'id',
+  label_prop: 'title',
+  descriptor_prop: 'details',
+  name: 'selected_id',
+  callback: function(obj) {
+    vhxm.models.section.chosen_item = { id: obj.value };
+  }
 });
+
