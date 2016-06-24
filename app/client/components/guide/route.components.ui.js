97ca5118fbc85e4073be75157756576e225fa686
Scott Robertson <scott@scottusrobus.com>
2016-06-24 07:45:14 -0700

    add components route file

---
+++ app/client/components/guide/route.components.ui.js
@@ -0,0 +1,9 @@
+// for route /js/components
+Q.components.route.js.ui.components = {
+  items: [
+    {
+      guide: m.component(Q.components.guide.js.ui.filter.container),
+      code: Q.code.filter
+    }
+  ]
+};
