c7ef8f1642ef6e094d97766b41eddd14036fe40f
Scott Robertson <scott@scottusrobus.com>
2016-07-07 06:45:23 -0700

    bump version to 0.8.3

--- distro/vhx-quartz.header.js
+++ distro/vhx-quartz.header.js
@@ -1,7 +1,9 @@
 'use strict';
 
 vhxm.components.shared.header.ui.container = {
-  view: function view(ctrl, data) {
-    return m('header.row.padding-bottom-medium.border-bottom', [m('.column.small-16.medium-8.large-10', [m('.media', [m('.media-unit.text-top', [m('i.icon.icon--large.icon-' + data.icon)]), m('.media-unit.media-fill.padding-left-medium', [m('h2.head-3', data.title), m('p.text.primary.small-margin-bottom-small', data.description ? data.description : '')])])]), m('.column.small-16.medium-8.large-6.text-right', data.buttons ? data.buttons : '')]);
+  view: function view(ctrl, opts) {
+    var has_border = opts.border === false ? false : true;
+
+    return m('header.row.padding-bottom-medium' + (has_border ? '.border-bottom' : ''), [m('.column.small-16.medium-8.large-10', [m('.media', [m('.media-unit.text-top', [m('i.icon.icon--large.icon-' + opts.icon)]), m('.media-unit.media-fill.padding-left-medium', [m('h2.head-3', opts.title), m('p.text.primary.small-margin-bottom-small', opts.description ? opts.description : '')])])]), m('.column.small-16.medium-8.large-6.text-right', opts.buttons ? opts.buttons : '')]);
   }
 };
--- distro/vhx-quartz.radio.js
+++ distro/vhx-quartz.radio.js
@@ -6,12 +6,13 @@ vhxm.components.shared.radio.ui.container = {
       return m('li', [m('input', {
         id: opts.name + '-' + index,
         type: 'radio',
+        onchange: opts.onchange,
         name: opts.name,
         value: item.value,
         checked: item.checked ? item.checked : null
       }), m('label', {
         for: opts.name + '-' + index
-      }, [m('span.radio--icon', [m('i.circle-top', [m('span')]), m('i.circle-bottom', [m('span')])]), m('span.radio--label', item.label)])]);
+      }, [m('span.radio--icon', [m('i.circle-top', [m('span')]), m('i.circle-bottom', [m('span')])]), m('span.radio--label', item.label)]), item.template ? m('div.clear.padding-top-xsmall', [item.template]) : '']);
     })])]);
   }
 };
