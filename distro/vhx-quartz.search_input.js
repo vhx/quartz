2b0e88cd4291be081569521630c4f1eb5b80936f
Scott Robertson <scott@scottusrobus.com>
2016-08-02 10:00:54 -0700

    bump version to 0.8.14

--- distro/vhx-quartz.search_input.js
+++ distro/vhx-quartz.search_input.js
@@ -4,6 +4,8 @@ vhxm.components.shared.search_input.controller = function (opts) {
   var self = this;
 
   self.timeout = null;
+  self.state = new vhxm.components.shared.search_input.state();
+  self.state.value(opts.value ? opts.value : null);
 
   self.debounceSearch = function (event) {
     if (self.timeout) {
@@ -25,6 +27,25 @@ vhxm.components.shared.search_input.controller = function (opts) {
       opts.callback(data);
     });
   };
+
+  self.updateQueryParam = function (query) {
+    var loc = window.location;
+    var search = void 0;
+    var href = loc.pathname;
+
+    if (query.length && window.history && window.history.replaceState) {
+      search = loc.search.length && loc.search.match(/\?/) ? loc.search : '?';
+      href += search.match(/q\=/) ? search.replace(/q\=(\w|\-)*/, 'q=' + query) : search + 'q=' + query;
+      history.replaceState({}, '', href);
+    } else {
+      search = loc.search.replace(/q\=(\w|\-)*/, '');
+      href += search.length === 1 ? '' : loc.search;
+      history.replaceState({}, '', href);
+    }
+  };
+};
+vhxm.components.shared.search_input.state = function () {
+  this.value = m.prop(null);
 };
 
 vhxm.components.shared.search_input.ui.container = {
@@ -35,8 +56,19 @@ vhxm.components.shared.search_input.ui.container = {
     return m('input.c-select--search.padding-right-large.icon-search-navy.icon--xsmall', {
       config: opts.config ? opts.config : '',
       type: 'text',
+      value: ctrl.state.value(),
       placeholder: opts.placeholder ? opts.placeholder : 'Search',
+      onkeydown: function oninput(event) {
+        if (parseInt(event.which, 10) === 13 || parseInt(event.keyCode, 10) === 13) {
+          ctrl.fetchResults(event.target.value);
+          ctrl.timeout = null;
+        }
+      },
       oninput: function oninput(event) {
+        ctrl.state.value(event.target.value);
+        if (opts.queryParam) {
+          ctrl.updateQueryParam(event.target.value);
+        }
         if (opts.oninput) {
           opts.oninput(event);
         }
