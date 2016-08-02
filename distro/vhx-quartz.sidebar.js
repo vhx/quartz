8f661860d299a97b016d4b3bee9bfede28080da7
Scott Robertson <scott@scottusrobus.com>
2016-08-02 12:46:47 -0700

    bump version to 0.8.15

--- distro/vhx-quartz.sidebar.js
+++ distro/vhx-quartz.sidebar.js
@@ -2,24 +2,34 @@
 
 vhxm.components.shared.sidebar.controller = function (opts) {
   var self = this;
+  self.opts = opts;
 
-  if (opts) {
-    if (opts.onInit) {
-      opts.onInit();
+  if (self.opts) {
+    if (self.opts.onInit) {
+      self.opts.onInit();
     }
-    if (opts.loadedState) {
-      vhxm.components.shared.sidebar.setLoadedState(opts.loadedState);
+    if (self.opts.loadedState) {
+      vhxm.components.shared.sidebar.setLoadedState(self.opts.loadedState);
     }
     if (opts.template) {
-      vhxm.components.shared.sidebar.setTemplate(opts.template);
+      vhxm.components.shared.sidebar.setTemplate(self.opts.template);
     }
     if (opts.onOpen) {
-      vhxm.components.shared.sidebar.state.onOpen = opts.onOpen;
+      vhxm.components.shared.sidebar.state.onOpen = self.opts.onOpen;
     }
     if (opts.onClose) {
-      vhxm.components.shared.sidebar.state.onClose = opts.onClose;
+      vhxm.components.shared.sidebar.state.onClose = self.opts.onClose;
     }
   }
+
+  self.documentClickHandler = function () {
+
+    var is_sidebar = !$(event.target).closest('body').length || $(event.target).hasClass('c-sidebar') || $(event.target).closest('[data-flash], tr.is-clickable, .c-sidebar' + (self.opts && self.opts.clickBlackList ? ', ' + self.opts.clickBlackList : '')).length;
+
+    if (!is_sidebar && vhxm.components.shared.sidebar.state.isOpen()) {
+      vhxm.components.shared.sidebar.toggle('close');
+    }
+  };
 
   self.animatorIn = function (elem, isInit) {
     if (vhxm.components.shared.sidebar.state.isOpen()) {
@@ -30,6 +40,7 @@ vhxm.components.shared.sidebar.controller = function (opts) {
         easing: [0.19, 1, 0.22, 1],
         complete: function complete() {
           $(document).on('keyup', self.esc);
+          $(document).on('click', self.documentClickHandler);
           vhxm.components.shared.sidebar.state.onOpen();
           vhxm.components.shared.sidebar.state.skipTransition(true);
         }
@@ -46,6 +57,7 @@ vhxm.components.shared.sidebar.controller = function (opts) {
       complete: function complete() {
         vhxm.components.shared.sidebar.state.onClose();
         $(document).off('keyup', self.esc);
+        $(document).off('click', self.documentClickHandler);
         vhxm.components.shared.sidebar.state.skipTransition(false);
       }
     });
