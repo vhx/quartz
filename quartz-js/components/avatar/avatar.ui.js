16c9fd789ec36433e986cf689b5bd8440ca225a3
Scott Robertson <scott@scottusrobus.com>
2016-08-03 13:46:23 -0700

    default to teal

--- quartz-js/components/avatar/avatar.ui.js
+++ quartz-js/components/avatar/avatar.ui.js
@@ -1,6 +1,6 @@
 vhxm.components.shared.avatar.ui = {
   view: function(ctrl, opts) {
-    return m('span.avatar.color-purple.avatar--' + (opts.size ? opts.size : 'medium' ), [
+    return m('span.avatar.color-teal.avatar--' + (opts.size ? opts.size : 'medium' ), [
       m('span.avatar-user.user-avatar', {
         style: {
           backgroundImage: 'url(' + opts.image + ')'
