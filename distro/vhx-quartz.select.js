3e2b64ad85890e1b6ed596b2aa1043734fceb3f7
Scott Robertson <scott@scottusrobus.com>
2016-09-20 09:58:13 -0700

    bump version to 0.10.5

--- distro/vhx-quartz.select.js
+++ distro/vhx-quartz.select.js
@@ -227,11 +227,11 @@ vhxm.components.shared.select.ui.container = {
       onkeydown: ctrl.handleKeydown
     }, [opts.trigger ? opts.trigger : m('a.c-select--trigger.btn-dropdown-' + (opts.color ? opts.color : 'gray') + '.btn--fill' + (ctrl.state.isDropdownOpen() ? '.is-active' : ''), {
       href: '#',
-      onclick: ctrl.handleClick,
+      onclick: ctrl.handleClick
+    }, ctrl.selectedLabel()), m('.c-select--dropdown.bg-white.border.radius.fill-width' + (ctrl.state.isDropdownOpen() ? '.is-open' : ''), {
       style: {
         top: opts.offset ? opts.offset + 'px' : '40px'
-      }
-    }, ctrl.selectedLabel()), m('.c-select--dropdown.bg-white.border.radius.fill-width' + (ctrl.state.isDropdownOpen() ? '.is-open' : ''), {
+      },
       config: function config(el) {
         if (ctrl.position === 'top') {
           el.style.top = -(el.offsetHeight + 10) + 'px';
