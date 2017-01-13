f1392f23fc2c2307b71d7bddb0d57c08bdb6b6b3
Steven Bone <steven@stevenbone.com>
2017-01-13 10:32:09 -0500

    add example form markup

--- quartz-css/vhx-style-forms/docs/guide.html.js
+++ quartz-css/vhx-style-forms/docs/guide.html.js
@@ -4,8 +4,54 @@ Q.components.guide.styleguide.ui.forms.container = {
   view: function(ctrl) {
     return m('section#guide--style-forms', [
       m.component(Q.components.shared.intro.ui.container, {
-        title: 'Forms'
-      })
+        title: 'Forms',
+        intro: 'Form styling is offered inside of a <code>form</code> with a <code>form</code> class'
+      }),
+      m.component(Q.components.shared.block.ui.container, {
+        title: 'Standard',
+        component: {
+          view: function() {
+            return m('form.form.border.radius.padding-medium', [
+              m('fieldset.margin-bottom-medium', [
+                m('label', {
+                  for: 'user_name'
+                }, 'User name'),
+                m('input', {
+                  type: 'text',
+                  name: 'user_name',
+                  placeholder: 'Enter a your user_name'
+                })
+              ]),
+              m('fieldset.margin-bottom-medium', [
+                m('label', {
+                  for: 'email'
+                }, 'Email'),
+                m('input', {
+                  type: 'email',
+                  name: 'email',
+                  placeholder: 'Enter your email'
+                })
+              ]),
+              m('fieldset.margin-bottom-medium', [
+                m('label', {
+                  for: 'message'
+                }, 'Enter your message'),
+                m('textarea', {
+                  type: 'message',
+                  name: 'message',
+                  placeholder: 'Enter a message'
+                })
+              ]),
+              m('.clearfix',
+                m('button.btn-teal.right', {
+                  type: 'submit'
+                }, 'Submit')
+              )
+            ]);
+          }
+        }
+      }),
+
     ]);
   }
 };
