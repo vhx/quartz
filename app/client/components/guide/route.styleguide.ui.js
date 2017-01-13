f6edbc71563c189f12334e1835a149105345aaeb
Steven Bone <steven@stevenbone.com>
2017-01-13 13:50:20 -0500

    Unify all style sections into one

---
+++ app/client/components/guide/route.styleguide.ui.js
@@ -0,0 +1,85 @@
+// for route /styleguide
+Q.components.route.styleguide.ui.all = {
+  items: [
+    {
+      guide: m.component(Q.components.guide.styleguide.ui.borders.container),
+      code: Q.code.borders
+    },
+    {
+      guide: m.component(Q.components.guide.styleguide.ui.buttons.container),
+      code: Q.code.buttons
+    },
+    {
+      guide: m.component(Q.components.guide.styleguide.ui.charts.container),
+      code: Q.code.charts
+    },
+    {
+      guide: m.component(Q.components.guide.styleguide.ui.colors_core.container),
+      code: Q.code['colors-core']
+    },
+    {
+      guide: m.component(Q.components.guide.styleguide.ui.colors_brand.container),
+      code: Q.code['colors-brand']
+    },
+    {
+      guide: m.component(Q.components.guide.styleguide.ui.colors_external.container),
+      code: Q.code['colors-external']
+    },
+    {
+      guide: m.component(Q.components.guide.styleguide.ui.colors_tools.container),
+      code: Q.code['colors-tools']
+    },
+    {
+      guide: m.component(Q.components.guide.styleguide.ui.checkbox.container),
+      code: Q.code['checkbox-ruby']
+    },
+    {
+      guide: m.component(Q.components.guide.styleguide.ui.radio.container),
+      code: Q.code['radio-ruby']
+    },
+    {
+      guide: m.component(Q.components.guide.styleguide.ui.grid.container),
+      code: Q.code.grid
+    },
+    {
+      guide: m.component(Q.components.guide.styleguide.ui.icons_styles.container),
+      code: Q.code['icon-styles']
+    },
+    {
+      guide: m.component(Q.components.guide.styleguide.ui.icons_names.container),
+      code: Q.code['icon-names']
+    },
+    {
+      guide: m.component(Q.components.guide.styleguide.ui.loaders.container),
+      code: Q.code.loaders
+    },
+    {
+      guide: m.component(Q.components.guide.styleguide.ui.display.container),
+      code: Q.code.display
+    },
+    {
+      guide: m.component(Q.components.guide.styleguide.ui.spacing_sizes.container),
+      code: Q.code['spacing-sizes']
+    },
+    {
+      guide: m.component(Q.components.guide.styleguide.ui.spacing_positions.container),
+      code: Q.code['spacing-positions']
+    },
+    {
+      guide: m.component(Q.components.guide.styleguide.ui.tables.container),
+      code: Q.code.tables
+    },
+    {
+      guide: m.component(Q.components.guide.styleguide.ui.tooltips.container),
+      code: Q.code.tooltips
+    },
+    {
+      guide: m.component(Q.components.guide.styleguide.ui.headers.container),
+      code: Q.code.headers
+    },
+    {
+      guide: m.component(Q.components.guide.styleguide.ui.text.container),
+      code: Q.code.text
+    },
+  ]
+};
