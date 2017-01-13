aa72adb998c5e394a2ce9a4d67ca21740299a83c
Steven Bone <steven@stevenbone.com>
2017-01-13 11:07:33 -0500

    Document additional `input` styles in forms
    - error
    - disabled
    - inner button
    - input size

--- quartz-css/vhx-style-forms/docs/guide.html.js
+++ quartz-css/vhx-style-forms/docs/guide.html.js
@@ -5,7 +5,7 @@ Q.components.guide.styleguide.ui.forms.container = {
     return m('section#guide--style-forms', [
       m.component(Q.components.shared.intro.ui.container, {
         title: 'Forms',
-        intro: 'Form styling is offered inside of a <code>form</code> with a <code>form</code> class'
+        intro: 'Form styling is offered inside of a <code>form</code> with a <code>form</code> class. Additional <code>input</code> styles are displayed below.'
       }),
       m.component(Q.components.shared.block.ui.container, {
         title: 'Standard',
@@ -24,12 +24,46 @@ Q.components.guide.styleguide.ui.forms.container = {
               ]),
               m('fieldset.margin-bottom-medium', [
                 m('label', {
-                  for: 'email'
-                }, 'Email'),
+                  for: 'secret_key'
+                }, 'Secret Key (disabled)'),
                 m('input', {
-                  type: 'email',
-                  name: 'email',
-                  placeholder: 'Enter your email'
+                  type: 'number',
+                  name: 'secret_key',
+                  disabled: true,
+                  placeholder: 'Enter your secret key (disabled)'
+                })
+              ]),
+              m('fieldset.margin-bottom-medium', [
+                m('label', {
+                  for: 'domain_name'
+                }, 'Domain Name (error)'),
+                m('input.is-error', {
+                  type: 'text',
+                  name: 'domain_name',
+                  placeholder: 'Enter your domain name (error)'
+                })
+              ]),
+              m('fieldset.margin-bottom-medium.inner-button', [
+                m('label', {
+                  for: 'domain_name'
+                }, 'Domain Name (inner button)'),
+                m('.inner-button', [
+                  m('input', {
+                    type: 'text',
+                    name: 'domain_name',
+                    placeholder: 'Enter your domain name'
+                  }),
+                  m('button.btn.btn-teal.btn--small', 'Verify Domain')
+                ])
+              ]),
+              m('fieldset.margin-bottom-medium', [
+                m('label', {
+                  for: 'state'
+                }, 'State (small input)'),
+                m('input.small', {
+                  type: 'text',
+                  name: 'state',
+                  placeholder: 'Enter your domain name (small input)'
                 })
               ]),
               m('fieldset.margin-bottom-medium', [
