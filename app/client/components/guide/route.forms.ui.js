8f10448484d1bdd8e8f8c1b19c29e0253ae5cafa
Steven Bone <steven@stevenbone.com>
2017-01-13 09:41:36 -0500

    Add Forms docs scaffolding and nav link

---
+++ app/client/components/guide/route.forms.ui.js
@@ -0,0 +1,9 @@
+// for route /styleguide/forms
+Q.components.route.styleguide.ui.forms = {
+  items: [
+    {
+      guide: m.component(Q.components.guide.styleguide.ui.forms.container),
+      code: Q.code.forms
+    }
+  ]
+};
