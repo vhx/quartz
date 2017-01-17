583dd597e5fac705a4f559d7ffe04cdfa676ea4e
Scott Robertson <scott@scottusrobus.com>
2017-01-17 13:36:54 -0800

    add display gudies

--- quartz-css/vhx-style-core/docs/display.guide.html.js
+++ quartz-css/vhx-style-core/docs/display.guide.html.js
@@ -13,8 +13,82 @@ Q.components.guide.styleguide.ui.display.container = {
             return m('.row.text-left', [
               m('span.text-center.margin-vert-medium.padding-vert-large.block.bg-gray-2.text-gray.text--small', 'Block'),
               m('span.margin-vert-medium.padding-medium.inline.bg-gray-2.text-gray.text--small', 'Inline Block'),
-              m('span.margin-vert-medium.padding-medium.inline.bg-gray-2.text-gray.text--small', 'Inline Block'),
-              m('span.margin-vert-medium.padding-medium.inline.bg-gray-2 text-gray.text--small', 'Inline Block')
+              m('span.radius.margin-vert-medium.padding-medium.block.bg-gray-2.text-gray.text--small', 'Rounded corners (3px radius)'),
+              m('span.shadow--gray.margin-vert-medium.padding-medium.block.bg-gray-2.text-gray.text--small', 'Box shadow'),
+            ]);
+          }
+        }
+      }),
+      m.component(Q.components.shared.block.ui.container, {
+        title: 'Alignment',
+        component: {
+          view: function() {
+            return m('.row.text-left', [
+              m('div.text-left', [
+                m('div.margin-vert-medium.padding-medium.inline.bg-gray-2.text-gray.text--small', 'Inline Block Left'),
+              ]),
+              m('div.text-center', [
+                m('div.margin-vert-medium.padding-medium.inline.bg-gray-2.text-gray.text--small', 'Inline Block Center'),
+              ]),
+              m('div.text-right', [
+                m('div.margin-vert-medium.padding-medium.inline.bg-gray-2.text-gray.text--small', 'Inline Block Right'),
+              ])
+            ]);
+          }
+        }
+      }),
+      m.component(Q.components.shared.block.ui.container, {
+        title: 'Arrows',
+        component: {
+          view: function() {
+            return m('div', [
+              m('p.margin-bottom-small', 'Colors can be set via the background class, which includes:'),
+              m('p.margin-bottom-medium', m.trust(
+              '<code class="block text--yellow">bg-navy</code>' +
+              '<code class="block text--yellow">bg-white</code>' +
+              '<code class="block text--yellow">bg-gray-1</code>' +
+              '<code class="block text--yellow">bg-gray-2</code>' +
+              '<code class="block text--yellow">bg-red</code>' +
+              '<code class="block text--yellow">bg-teal-1</code>',
+              '<code class="block text--yellow">bg-teal-2</code>')),
+              m('div.row', [
+                m('div.col.small-thirds', [
+                  m('div.margin-small.arrow.arrow-top-left.text-center.padding-small.block.bg-gray-2.text-gray.text--small', 'Top left')
+                ]),
+                m('div.col.small-thirds', [
+                  m('div.margin-small.arrow.arrow-top-middle.text-center.padding-small.block.bg-gray-2.text-gray.text--small', 'Top middle')
+                ]),
+                m('div.col.small-thirds', [
+                  m('div.margin-small.arrow.arrow-top-right.text-center.padding-small.block.bg-gray-2.text-gray.text--small', 'Top right')
+                ]),
+                m('div.col.small-thirds', [
+                  m('div.margin-small.arrow.arrow-right-top.text-center.padding-small.block.bg-gray-2.text-gray.text--small', 'Right top')
+                ]),
+                m('div.col.small-thirds', [
+                  m('div.margin-small.arrow.arrow-right-middle.text-center.padding-small.block.bg-gray-2.text-gray.text--small', 'Right middle')
+                ]),
+                m('div.col.small-thirds', [
+                  m('div.margin-small.arrow.arrow-right-bottom.text-center.padding-small.block.bg-gray-2.text-gray.text--small', 'Right bottom')
+                ]),
+                m('div.col.small-thirds', [
+                  m('div.margin-small.arrow.arrow-bottom-left.text-center.padding-small.block.bg-gray-2.text-gray.text--small', 'Bottom left')
+                ]),
+                m('div.col.small-thirds', [
+                  m('div.margin-small.arrow.arrow-bottom-middle.text-center.padding-small.block.bg-gray-2.text-gray.text--small', 'Bottom middle')
+                ]),
+                m('div.col.small-thirds', [
+                  m('div.margin-small.arrow.arrow-bottom-right.text-center.padding-small.block.bg-gray-2.text-gray.text--small', 'Bottom right')
+                ]),
+                m('div.col.small-thirds', [
+                  m('div.margin-small.arrow.arrow-left-top.text-center.padding-small.block.bg-gray-2.text-gray.text--small', 'Left top')
+                ]),
+                m('div.col.small-thirds', [
+                  m('div.margin-small.arrow.arrow-left-middle.text-center.padding-small.block.bg-gray-2.text-gray.text--small', 'Left middle')
+                ]),
+                m('div.col.small-thirds', [
+                  m('div.margin-small.arrow.arrow-left-bottom.text-center.padding-small.block.bg-gray-2.text-gray.text--small', 'Left bottom')
+                ])
+              ])
             ]);
           }
         }
@@ -36,7 +110,13 @@ Q.components.guide.styleguide.ui.display.container = {
                 m('li.border', 'Item'),
                 m('li.border', 'Item'),
                 m('li.border', 'Item')
-              ])
+              ]),
+              m('p.margin-bottom-medium.margin-top-large', 'Inline list striped rows.'),
+              m('ul.striped', [
+                m('li', 'Item'),
+                m('li', 'Item'),
+                m('li', 'Item'),
+              ]),
             ]);
           }
         }
