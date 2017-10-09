12f487519304240a64ee6ee3c090938f65fe10b8
Hannah Nordgren <hey@hereshannahs.info>
2017-10-09 11:43:45 -0400

    [FEATURE] updated c-tag button with vimeo colors

--- dist/component-tag.js
+++ dist/component-tag.js
@@ -42,12 +42,12 @@ vhxm.components.shared.tag.ui.container = {
       onmouseout: function onmouseout() {
         ctrl.state.isHover(false);
       }
-    }, [m('button.c-tag--button' + (ctrl.state.isHover() ? '.btn-vimeo-blue.is-hover' : '.btn-vimeo-secondary') + (ctrl.state.isProcessing() ? '.is-processing' : ''), {
+    }, [m('button.c-tag--button' + (ctrl.state.isHover() ? '.btn-teal.is-hover' : '.btn-gray') + (ctrl.state.isProcessing() ? '.is-processing' : ''), {
       onclick: function onclick(event) {
         event.preventDefault();
         ctrl.state.onShow(event);
       }
-    }, opts.label ? ctrl.formatLabelString(opts) : 'Tag'), m('a.c-tag--remove.icon--center.icon-x-white.icon--xxsmall' + (ctrl.state.isRemoveHover() ? '.btn-vimeo-red' : '.btn-vimeo-blue'), {
+    }, opts.label ? ctrl.formatLabelString(opts) : 'Tag'), m('a.c-tag--remove.icon--center.icon-x-white.icon--xxsmall' + (ctrl.state.isRemoveHover() ? '.btn-red' : '.btn-teal'), {
       onclick: function onclick(event) {
         event.preventDefault();
         ctrl.state.onRemove(event);
