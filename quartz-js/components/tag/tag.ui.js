2ca9ea2426edf7aa6b09d4dfdfe39a1f2df2e5b1
Hannah Nordgren <hey@hereshannahs.info>
2017-10-09 12:09:56 -0400

    [BUGFIX] updated actual colors of tags

--- quartz-js/components/tag/tag.ui.js
+++ quartz-js/components/tag/tag.ui.js
@@ -9,13 +9,13 @@ vhxm.components.shared.tag.ui.container = {
         ctrl.state.isHover(false);
       }
     }, [
-      m('button.c-tag--button' + (ctrl.state.isHover() ? '.btn-teal.is-hover' : '.btn-gray') + (ctrl.state.isProcessing() ? '.is-processing': ''), {
+      m('button.c-tag--button' + (ctrl.state.isHover() ? '.btn-vimeo-blue.is-hover' : '.btn-vimeo-secondary') + (ctrl.state.isProcessing() ? '.is-processing': ''), {
         onclick: function(event) {
           event.preventDefault();
           ctrl.state.onShow(event);
         }
       }, (opts.label ? ctrl.formatLabelString(opts) : 'Tag')),
-      m('a.c-tag--remove.icon--center.icon-x-white.icon--xxsmall' + (ctrl.state.isRemoveHover() ? '.btn-red' : '.btn-teal'), {
+      m('a.c-tag--remove.icon--center.icon-x-white.icon--xxsmall' + (ctrl.state.isRemoveHover() ? '.btn-vimeo-red' : '.btn-vimeo-blue'), {
         onclick: function(event) {
           event.preventDefault();
           ctrl.state.onRemove(event);
