5bd8c0ba8134db79d78c1cc2497726b167449f5f
Erik Moldovan <erikmoldovan@users.noreply.github.com>
2018-02-27 17:11:52 -0500

    VHX-1429 : Adding default avatar functionality to quartz (#99)

--- quartz-js/components/avatar/avatar.ui.js
+++ quartz-js/components/avatar/avatar.ui.js
@@ -1,6 +1,7 @@
 vhxm.components.shared.avatar.ui = {
   view: function(ctrl, opts) {
-    return m('span.avatar.color-teal.avatar--' + (opts.size ? opts.size : 'medium' ), [
+    return m('span.new-avatar.color-teal.avatar--' + (opts.size ? opts.size : 'medium' ), [
+      m('span.default-avatar', opts.initial),
       m('span.avatar-user.user-avatar', {
         style: {
           backgroundImage: 'url(' + opts.image + ')'
--- quartz-js/components/avatar/docs/guide.html.js
+++ quartz-js/components/avatar/docs/guide.html.js
@@ -4,7 +4,8 @@ Q.components.guide.js.ui.avatar.container = {
     return m('section#guide--component-avatar', [
       m.component(Q.components.shared.intro.ui.container, {
         title: 'Avatar',
-        intro: 'User avatar.'
+        intro: 'User avatar.',
+        initial: 'A',
       }),
       m.component(Q.components.shared.block.ui.container, {
         title: 'Examples',
@@ -14,24 +15,28 @@ Q.components.guide.js.ui.avatar.container = {
               m('span.inline.padding-right-small', [
                 m.component(vhxm.components.shared.avatar.ui, {
                   size: 'small',
+                  initial: 'A',
                 })
               ]),
               m('span.inline.padding-right-small', [
                 m.component(vhxm.components.shared.avatar.ui, {
                   size: 'medium',
                   image: 'https://secure.gravatar.com/avatar/74b978ed4f10e05a6c2898c4f5516189.png',
+                  initial: 'A',
                 })
               ]),
               m('span.inline.padding-right-small', [
                 m.component(vhxm.components.shared.avatar.ui, {
                   size: 'large',
                   image: 'https://secure.gravatar.com/avatar/460c13d4904dc7f889f00d2cd4c3e6e8.png',
+                  initial: 'A',
                 })
               ]),
               m('span.inline.padding-right-small', [
                 m.component(vhxm.components.shared.avatar.ui, {
                   size: 'xlarge',
-                  image: 'https://vhx.imgix.net/assets/1f843fdc-34da-4302-bd4f-06e5bd7ef2c3/IMG_7891.GIF'
+                  image: 'https://vhx.imgix.net/assets/1f843fdc-34da-4302-bd4f-06e5bd7ef2c3/IMG_7891.GIF',
+                  initial: 'A',
                 })
               ])
             ]);
