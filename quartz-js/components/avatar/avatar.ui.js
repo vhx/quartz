13a15c86ef173a705d5929459a15a950070824be
Erik Moldovan <erikmoldovan@gmail.com>
2018-03-05 13:50:48 -0500

    And adding some generated code

--- quartz-js/components/avatar/avatar.ui.js
+++ quartz-js/components/avatar/avatar.ui.js
@@ -1,12 +1,14 @@
 vhxm.components.shared.avatar.ui = {
   view: function(ctrl, opts) {
     return m('span.new-avatar.color-teal.avatar--' + (opts.size ? opts.size : 'medium' ), [
-      m('span.default-avatar', opts.initial),
-      m('span.avatar-user.user-avatar', {
-        style: {
-          backgroundImage: 'url(' + opts.image + ')'
-        }
-      })
+      m('span.avatar-user.user-avatar', [
+        m('span.default-avatar', opts.initial), 
+        m('span.avatar-user.user-avatar', {
+          style: {
+            backgroundImage: 'url(' + opts.image + ')'
+          }
+       })
+      ])
     ]);
   }
 };
